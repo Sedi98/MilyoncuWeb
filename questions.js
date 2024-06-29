207b15c2e2d545eb4b1cf4fc73d08779555aa849
Sedi98 <sedi9816@gmail.com>
2024-06-29 15:15:43 +0400

    first commit

---
+++ questions.js
@@ -0,0 +1,506 @@
+let questions = [
+  {
+    question:
+      "Veb saytın ilkin strukturu (skeleti) hansı texnologiya istifadə edərək hazırlanır?",
+    answer: "HTML",
+    options: ["HTML", "CSS", "JAVASCRİPT", "REACT"],
+  },
+  {
+    question:
+      "İki array data tipini bir birinə birləşdirmək üçün hansı metoddan istifadə edilir?",
+    answer: "concat()",
+    options: ["push()", "pop()", "concat()", "unshift()"],
+  },
+  {
+    question:
+      "Javascript istifadə edərək backend yazmaq üçün nədən istifadə edilir?",
+    answer: "NodeJS",
+    options: ["CodeJS", "MockJS", "ReactJS", "NodeJS"],
+  },
+  {
+    question: "HTML-də hansı teq istifadə edilərək link yaradılır?",
+    answer: "a",
+    options: ["div", "link", "a", "span"],
+  },
+  {
+    question:
+      "CSS-də hansı property istifadə edilərək elementin fon rəngi təyin edilir?",
+    answer: "background-color",
+    options: ["color", "background", "background-color", "font-color"],
+  },
+  {
+    question:
+      "JavaScript-də hansı operator istifadə edilərək bərabərlik yoxlanılır?",
+    answer: "==",
+    options: ["=", "===", "==", "!="],
+  },
+  {
+    question: "HTML-də hansı teq istifadə edilərək şəkil əlavə edilir?",
+    answer: "img",
+    options: ["src", "image", "picture", "img"],
+  },
+  {
+    question:
+      "CSS-də hansı property istifadə edilərək elementin uzunluğu təyin edilir?",
+    answer: "width",
+    options: ["size", "width", "height", "dimension"],
+  },
+  {
+    question:
+      "JavaScript-də hansı funksiya istifadə edilərək ekrana xəbərdarlıq mesajı göstərilir?",
+    answer: "alert()",
+    options: ["console.log()", "alert()", "prompt()", "document.write()"],
+  },
+  {
+    question: "HTML-də hansı teq istifadə edilərək başlıq mətni yazılır?",
+    answer: "h1",
+    options: ["header", "h1", "head", "title"],
+  },
+  {
+    question:
+      "CSS-də hansı property istifadə edilərək elementin kənar sərhəd xətti təyin edilir?",
+    answer: "border",
+    options: ["border", "outline", "line", "frame"],
+  },
+  {
+    question:
+      "JavaScript-də hansı metod istifadə edilərək stringin uzunluğu əldə edilir?",
+    answer: "length",
+    options: ["size", "length", "count", "substring"],
+  },
+  {
+    question: "HTML-də hansı teq istifadə edilərək siyahı yaradılır?",
+    answer: "ul",
+    options: ["ul", "ol", "list", "li"],
+  },
+  {
+    question:
+      "CSS-də hansı property istifadə edilərək elementin mətn ölçüsü təyin edilir?",
+    answer: "font-size",
+    options: ["text-size", "font-size", "size", "text-dimension"],
+  },
+  {
+    question:
+      "JavaScript-də hansı metod istifadə edilərək arrayin sonuna element əlavə edilir?",
+    answer: "push()",
+    options: ["add()", "push()", "append()", "insert()"],
+  },
+  {
+    question: "HTML-də hansı teq istifadə edilərək paragraf yazılır?",
+    answer: "p",
+    options: ["paragraph", "text", "p", "para"],
+  },
+  {
+    question:
+      "CSS-də hansı property istifadə edilərək elementin görünməsi/yox olması təyin edilir?",
+    answer: "display",
+    options: ["visibility", "display", "show", "hidden"],
+  },
+  {
+    question:
+      "JavaScript-də hansı metod istifadə edilərək bir stringin başqa stringə çevrilməsi mümkündür?",
+    answer: "replace()",
+    options: ["replace()", "convert()", "change()", "transform()"],
+  },
+  {
+    question: "HTML-də hansı teq istifadə edilərək düymə yaradılır?",
+    answer: "button",
+    options: ["input", "button", "submit", "click"],
+  },
+  {
+    question:
+      "CSS-də hansı property istifadə edilərək elementin daxili boşluğu təyin edilir?",
+    answer: "padding",
+    options: ["margin", "padding", "space", "inner-space"],
+  },
+  {
+    question:
+      "JavaScript-də hansı operator istifadə edilərək məntiqi bərabərlik yoxlanılır? (value and type) ",
+    answer: "===",
+    options: ["=", "==", "===", "!=="],
+  },
+  {
+    question: "HTML-də hansı teq istifadə edilərək cədvəl yaradılır?",
+    answer: "table",
+    options: ["tr", "td", "table", "tbody"],
+  },
+  {
+    question:
+      "CSS-də hansı property istifadə edilərək elementin xarici boşluğu təyin edilir?",
+    answer: "margin",
+    options: ["padding", "space", "outer-space", "margin"],
+  },
+  {
+    question:
+      "JavaScript-də hansı metod istifadə edilərək arrayin ilk elementini silmək mümkündür?",
+    answer: "shift()",
+    options: ["unshift()", "shift()", "pop()", "splice()"],
+  },
+  {
+    question: "HTML-də hansı teq istifadə edilərək link açılır?",
+    answer: "a",
+    options: ["link", "href", "a", "url"],
+  },
+  {
+    question:
+      "CSS-də hansı property istifadə edilərək elementin şrift ailəsi təyin edilir?",
+    answer: "font-family",
+    options: ["font", "font-family", "text-family", "font-type"],
+  },
+  {
+    question:
+      "JavaScript-də hansı metod istifadə edilərək bir stringin başlanğıcından boşluqlar silinir?",
+    answer: "trimStart()",
+    options: ["trim()", "trimEnd()", "trimStart()", "cutStart()"],
+  },
+  {
+    question: "HTML-də hansı teq istifadə edilərək şəkil əlavə edilir?",
+    answer: "img",
+    options: ["picture", "photo", "img", "image"],
+  },
+  {
+    question:
+      "CSS-də hansı property istifadə edilərək elementin mətn xətti hündürlüyü təyin edilir?",
+    answer: "line-height",
+    options: ["height", "text-height", "line-height", "font-height"],
+  },
+  {
+    question:
+      "JavaScript-də hansı metod istifadə edilərək arrayin son elementini silmək mümkündür?",
+    answer: "pop()",
+    options: ["push()", "shift()", "pop()", "unshift()"],
+  },
+  {
+    question: "HTML-də hansı teq istifadə edilərək formlar yaradılır?",
+    answer: "form",
+    options: ["input", "submit", "form", "button"],
+  },
+  {
+    question:
+      "CSS-də hansı property istifadə edilərək elementin yerləşmə yeri təyin edilir?",
+    answer: "position",
+    options: ["position", "location", "align", "place"],
+  },
+  {
+    question:
+      "JavaScript-də hansı metod istifadə edilərək stringin kiçik hərflərə çevrilməsi mümkündür?",
+    answer: "toLowerCase()",
+    options: [
+      "toUpperCase()",
+      "toLower()",
+      "toLowerCase()",
+      "convertToLower()",
+    ],
+  },
+  {
+    question: "HTML-də hansı teq istifadə edilərək video əlavə edilir?",
+    answer: "video",
+    options: ["media", "movie", "video", "clip"],
+  },
+  {
+    question:
+      "CSS-də hansı property istifadə edilərək elementin keçid effekti təyin edilir?",
+    answer: "transition",
+    options: ["transform", "animation", "transition", "effect"],
+  },
+
+  {
+    question: "HTML-də hansı teq istifadə edilərək audio əlavə edilir?",
+    answer: "audio",
+    options: ["sound", "music", "voice", "audio"],
+  },
+  {
+    question:
+      "CSS-də hansı property istifadə edilərək elementin kölgəsi təyin edilir?",
+    answer: "box-shadow",
+    options: ["box-shadow", "shadow", "filter", "opacity"],
+  },
+  {
+    question:
+      "JavaScript-də hansı metod istifadə edilərək arrayın elementlərinin təyin edilən ardıcıllıqla sıralanması mümkündür?",
+    answer: "sort()",
+    options: ["order()", "filter()", "sort()", "map()"],
+  },
+  {
+    question: "HTML-də hansı teq istifadə edilərək cədvəl setırı yaradılır?",
+    answer: "tr",
+    options: ["tr", "table", "td", "th"],
+  },
+  {
+    question:
+      "CSS-də hansı property istifadə edilərək elementin ölçüsünün(vidth) avtomatik olması təyin edilir?",
+    answer: "auto",
+    options: ["inherit", "default", "auto", "initial"],
+  },
+  {
+    question: "HTML-də hansı teq istifadə edilərək input sahəsi yaradılır?",
+    answer: "input",
+    options: ["input", "form", "textbox", "field"],
+  },
+  {
+    question:
+      "CSS-də hansı property istifadə edilərək elementin şəffaflığı təyin edilir?",
+    answer: "opacity",
+    options: ["opacity", "transparency", "visibility", "alpha"],
+  },
+  {
+    question:
+      "JavaScript-də hansı metod istifadə edilərək bir arrayin elementlərini stringə çevirmək mümkündür?",
+    answer: "join()",
+    options: ["concat()", "combine()", "merge()", "join()"],
+  },
+
+  ,
+  {
+    question:
+      "JavaScript-də hansı metod istifadə edilərək bir stringin müəyyən bir hissəsinin dəyişdirilməsi mümkündür?",
+    answer: "replace()",
+    options: ["edit()", "replace()", "change()", "transform()"],
+  },
+
+  {
+    question:
+      "CSS-də hansı property istifadə edilərək elementin kənarından boşluq təyin edilir?",
+    answer: "margin",
+    options: ["margin", "padding", "spacing", "border"],
+  },
+
+  {
+    question:
+      "HTML-də hansı teq istifadə edilərək siyahı elementləri yaradılır?",
+    answer: "li",
+    options: ["li", "ul", "ol", "list"],
+  },
+  {
+    question:
+      "CSS-də hansı property istifadə edilərək elementin keçidləri təyin edilir?",
+    answer: "transition",
+    options: ["transition", "animation", "transform", "effect"],
+  },
+
+  {
+    question:
+      "CSS-də hansı xassə istifadə edilərək elementin yüksəkliyi təyin edilir?",
+    answer: "height",
+    options: ["width", "length", "height", "size"],
+  },
+  {
+    question:
+      "JavaScript-də hansı metod istifadə edilərək stringin hərfləri böyük hərflərə çevrilir?",
+    answer: "toUpperCase()",
+    options: ["toUpperCase()", "toLowerCase()", "toCaps()", "convertToUpper()"],
+  },
+  {
+    question: "HTML-də hansı teq istifadə edilərək mətn italic yazılır?",
+    answer: "i",
+    options: ["em", "i", "italic", "strong"],
+  },
+  {
+    question:
+      "CSS-də hansı xassə istifadə edilərək elementin mətn üslubu təyin edilir?",
+    answer: "font-style",
+    options: ["text-style", "font-weight", "text-weight", "font-style"],
+  },
+
+  {
+    question:
+      "CSS-də hansı xassə istifadə edilərək elementin şrift rəngi təyin edilir?",
+    answer: "color",
+    options: ["font-color", "text-color", "color", "foreground-color"],
+  },
+
+  {
+    question: "HTML-də hansı teq istifadə edilərək horizontal xətt çəkilir?",
+    answer: "hr",
+    options: ["line", "horizontal", "br", "hr"],
+  },
+  {
+    question:
+      "CSS-də hansı xassə istifadə edilərək elementin xətt bəzəyi təyin edilir?",
+    answer: "text-decoration",
+    options: ["text-style", "text-format", "text-decoration", "text-transform"],
+  },
+
+  {
+    question:
+      "HTML-də hansı teq istifadə edilərək meta məlumatlar əlavə edilir?",
+    answer: "meta",
+    options: ["meta", "head", "link", "script"],
+  },
+  {
+    question:
+      "CSS-də hansı xassə istifadə edilərək elementin sərhəd xətti genişliyi təyin edilir?",
+    answer: "border-width",
+    options: ["border-size", "border-width", "border-height", "border-length"],
+  },
+  {
+    question:
+      "JavaScript-də hansı metod istifadə edilərək stringdən boşluqlar silinir?",
+    answer: "trim()",
+    options: ["cut()", "strip()", "trim()", "remove()"],
+  },
+  {
+    question:
+      "HTML-də hansı teq istifadə edilərək java script kodları səhifəyə  əlavə edilir?",
+    answer: "script",
+    options: ["link", "meta", "script", "style"],
+  },
+  {
+    question:
+      "CSS-də hansı xassə istifadə edilərək elementin sərhəd xətti stili təyin edilir?",
+    answer: "border-style",
+    options: ["border-type", "border-format", "border-pattern", "border-style"],
+  },
+  {
+    question:
+      "JavaScript-də hansı metod istifadə edilərək bir arrayin elementlərini tərsinə çevirmək mümkündür?",
+    answer: "reverse()",
+    options: ["invert()", "flip()", "reverse()", "turn()"],
+  },
+  {
+    question:
+      "HTML-də hansı teq istifadə edilərək html kodları arasında css yazmaq mümkündür?",
+    answer: "style",
+    options: ["css", "link", "style", "script"],
+  },
+  
+  {
+    question:
+      "JavaScript-də hansı metod istifadə edilərək bir arrayin müəyyən elementlərini return etmək mümkündür?",
+    answer: "filter()",
+    options: ["filter()", "map()", "reduce()", "find()"],
+  },
+  {
+    question: "HTML-də hansı teq istifadə edilərək əlaqə forması yaradılır?",
+    answer: "form",
+    options: ["form", "input", "button", "textarea"],
+  },
+  {
+    question:
+      "CSS-də hansı xassə istifadə edilərək elementin şrift ölçüsü təyin edilir?",
+    answer: "font-size",
+    options: ["font-size", "text-size", "font-height", "text-height"],
+  },
+  {
+    question:
+      "JavaScript-də hansı metod istifadə edilərək bir stringin uzunluğu əldə edilir?",
+    answer: "length",
+    options: ["length", "size", "count", "measure"],
+  },
+
+  {
+    question:
+      "CSS-də hansı xassə istifadə edilərək elementin rəngi təyin edilir?",
+    answer: "color",
+    options: ["color", "font-color", "text-color", "background-color"],
+  },
+  {
+    question:
+      "JavaScript-də hansı metod istifadə edilərək bir arrayin elementlərinin sayı təyin edilir?",
+    answer: "length",
+    options: ["length", "size", "count", "measure"],
+  },
+  {
+    question:
+      "HTML-də hansı teq istifadə edilərək bir səhifəyə başqa bir səhifə əlavə edilir?",
+    answer: "iframe",
+    options: ["iframe", "frame", "embed", "object"],
+  },
+
+  {
+    question:
+      "JavaScript-də hansı metod istifadə edilərək stringin müəyyən bir hissəsi dəyişdirilir?",
+    answer: "replace()",
+    options: ["replace()", "change()", "edit()", "update()"],
+  },
+
+  {
+    question:
+      "CSS-də hansı xassə istifadə edilərək elementin mətnin yönləndirilməsi təyin edilir?",
+    answer: "text-align",
+    options: ["text-align", "align", "text-direction", "text-position"],
+  },
+  {
+    question:
+      "JavaScript-də hansı metod istifadə edilərək bir arrayın tərkibini yoxlamaq mümkündür?",
+    answer: "includes()",
+    options: ["includes()", "contains()", "find()", "has()"],
+  },
+  {
+    question: "HTML-də hansı teq istifadə edilərək başlıq yaradılır?",
+    answer: "h1",
+    options: ["h1", "header", "title", "heading"],
+  },
+
+  {
+    question:
+      "JavaScript-də hansı metod istifadə edilərək stringin başlanğıcında boşluqlar silinir?",
+    answer: "trimStart()",
+    options: ["trimStart()", "trim()", "cutStart()", "stripStart()"],
+  },
+  {
+    question: "HTML-də hansı teq istifadə edilərək link yaradılır?",
+    answer: "a",
+    options: ["a", "link", "href", "anchor"],
+  },
+  {
+    question:
+      "CSS-də hansı xassə istifadə edilərək elementin yer dəyişməsi təyin edilir?",
+    answer: "position",
+    options: ["position", "location", "place", "align"],
+  },
+  {
+    question:
+      "JavaScript-də hansı metod istifadə edilərək arrayın hər bir elementi üçün bir funksiya tətbiq edilir?",
+    answer: "forEach()",
+    options: ["forEach()", "map()", "reduce()", "filter()"],
+  },
+  {
+    question: "HTML-də hansı teq istifadə edilərək siyahı elementi yaradılır?",
+    answer: "li",
+    options: ["li", "ul", "ol", "list-item"],
+  },
+  {
+    question:
+      "CSS-də hansı xassə istifadə edilərək elementin xətt hündürlüyü təyin edilir?",
+    answer: "line-height",
+    options: ["line-height", "height", "font-height", "text-height"],
+  },
+  {
+    question:
+      "JavaScript-də hansı metod istifadə edilərək arrayın sonuna element əlavə edilir?",
+    answer: "push()",
+    options: ["push()", "pop()", "add()", "append()"],
+  },
+  {
+    question:
+      "HTML-də hansı teq istifadə edilərək mətn daxilində bölmə yaradılır?",
+    answer: "span",
+    options: ["span", "div", "section", "article"],
+  },
+  {
+    question:
+      "CSS-də hansı xassə istifadə edilərək elementin sərhəd radiusu təyin edilir?",
+    answer: "border-radius",
+    options: ["border-radius", "radius", "border-width", "border-style"],
+  },
+  {
+    question:
+      "JavaScript-də hansı metod istifadə edilərək bir arrayin əvvəlinə element əlavə edilir?",
+    answer: "unshift()",
+    options: ["unshift()", "push()", "add()", "prepend()"],
+  },
+
+  {
+    question:
+      "CSS-də hansı xassə istifadə edilərək elementin mətn kölgəsi təyin edilir?",
+    answer: "text-shadow",
+    options: ["text-shadow", "shadow", "box-shadow", "opacity"],
+  },
+  {
+    question:
+      "JavaScript-də hansı metod istifadə edilərək stringin müəyyən bir hissəsi əldə edilir?",
+    answer: "substring()",
+    options: ["substring()", "slice()", "substr()", "cut()"],
+  },
+];
---
+++ script.js
@@ -0,0 +1,144 @@
+// musics
+let newQuestionMusic = new Audio("./assets/sounds/newQuestion.m4a");
+let checkAnswerMusic = new Audio("./assets/sounds/finalAnswer.m4a");
+let trueAnswerMusic = new Audio("./assets/sounds/trueAnswer.m4a");
+let wrongAnswerMusic = new Audio("./assets/sounds/wrongAnswer.m4a");
+let timeoutMusic = new Audio("./assets/sounds/timeoutMusic.m4a");
+
+let timeoutText = document.querySelector(".timeoutText");
+
+let moneyText = document.querySelector(".moneyText");
+
+let questIndex = 0;
+let money = 0;
+let time = 30;
+let timer;
+let variantSelected = false;
+
+let strtBtn = document.querySelector(".strtBtn");
+let contentQuestions = document.querySelector(".contentQuestions");
+
+function getAnswer(data) {
+  if (!variantSelected) {
+    variantSelected = true;
+    data.style.backgroundColor = "#3e74b5";
+    data.style.color = "#fff";
+    itemId = data.id;
+    // this.classList.add("active");
+    newQuestionMusic.pause();
+    newQuestionMusic.currentTime = 0;
+    checkAnswerMusic.play();
+    stopTime();
+
+    let timeout = setTimeout(() => {
+      // this.classList.remove("active");
+      console.log("happen after 3 sec");
+      checkAnswerMusic.pause();
+      checkAnswerMusic.currentTime = 0;
+      checkAnswer(questIndex, data.innerHTML, data);
+    }, 6000);
+  } else{
+    return
+  }
+}
+
+function checkAnswer(index, answer, html) {
+    
+  console.log(index, answer);
+  let correctAnswer = questions[index].answer;
+  if (answer == correctAnswer) {
+    html.style.backgroundColor = "#148f62";
+    trueAnswerMusic.play();
+    money += 10;
+    console.log(money);
+    moneyText.innerHTML = `${money} ₼`;
+    console.log("it is true answer");
+    questions.splice(index, 1);
+    stopTime();
+    setTimeout(() => {
+      newQuestion();
+      variantSelected = false;
+    }, 5000);
+  } else {
+    wrongAnswerMusic.play();
+    console.log(correctAnswer);
+    html.style.backgroundColor = "brown";
+    setTimeout(() => {
+      endGame();
+    }, 3000);
+  }
+}
+
+function startRace() {
+  let userName = prompt("Zehmet olmasa adinizi daxil edin");
+
+  if (userName) {
+    newQuestion();
+  } else {
+    return;
+  }
+}
+
+function startTime() {
+  timeoutText.style.visibility = "visible";
+  moneyText.style.visibility = "visible";
+  timeoutMusic.play();
+
+  timer = setInterval(() => {
+    if (time > 0) {
+      time--;
+      timeoutText.innerHTML = time;
+    } else {
+      clearInterval(timer);
+      timeoutMusic.pause();
+      timeoutMusic.currentTime = 0;
+      timeoutText.innerHTML = 0;
+
+      endGame();
+    }
+  }, 1000);
+}
+
+function stopTime() {
+  clearInterval(timer);
+  timeoutMusic.pause();
+  timeoutMusic.currentTime = 0;
+  timeoutText.innerHTML = time;
+}
+
+function endGame() {
+    variantSelected = false;
+  alert(`Oyun bitdi. Məbləğiniz: ${money} ₼`);
+  money = 0;
+}
+
+function newQuestion() {
+  time = 30;
+  startTime();
+  let randomIndex = Math.floor(Math.random() * (questions.length - 1));
+  console.log(randomIndex);
+  let newQuest = questions[randomIndex];
+  questIndex = randomIndex;
+  contentQuestions.innerHTML = `
+    <div class="questionCnt">
+          <h1 class="question">
+            ${newQuest.question}
+          </h1>
+        </div>
+        
+
+        <div class="answers">
+          <div class="answerVariant" id="a" onclick="getAnswer(this)">${newQuest.options[0]}</div>
+          <div class="answerVariant" id="b" onclick="getAnswer(this)">${newQuest.options[1]}</div>
+          <div class="answerVariant" id="c" onclick="getAnswer(this)">${newQuest.options[2]}</div>
+          <div class="answerVariant" id="d" onclick="getAnswer(this)">${newQuest.options[3]}</div>
+        </div>`;
+
+  newQuestionMusic.play();
+  trueAnswerMusic.pause();
+  trueAnswerMusic.currentTime = 0;
+}
+
+// startRace();
+
+strtBtn.addEventListener("click", startRace);
